3c2665c2d26f9abf396e2a982238d55972b2dc14
Kai Feldhoff <kaifeldhoff@yahoo.de>
2024-04-10 17:38:05 +0200

    mix format

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule WorkflowTest.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
